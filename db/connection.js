db14ffd778b2442672aa615bd9d871794dc6402f
gato365 <immanuelwilliams@gato365.com>
2022-12-31 14:37:01 -0800

    Modify methods and added update to connection class

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql2");
+
+// Develop Connection
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
--- db/index.js
+++ db/index.js
@@ -3,47 +3,52 @@
 const connection = require('./connection');
 
 class DB {
-    constructor(connection){
+    constructor(connection) {
         this.connection = connection;
     }
-    
-    //1. viewDepartment: Department
-    findAllDepartment(){
-        return this.connection.promise().query( 
-            "SELECT * from department ") ;
-        
-    }
-    //2. viewRole: Role
-    findAllRoles(){
-        return this.connection.promise().query( 
-            "SELECT * from role") ;
-        
-    }
-    //3. viewEmployee
-    findAllEmployees(){
-        return this.connection.promise().query( 
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
 
-            
-            
-            ) ;
-        
+    //1. Department
+    findAllDepartment() {
+        return this.connection.promise().query(
+            "SELECT * from department ");
+
     }
-    //4. addDepartment
-    createDepartment(department){
-        return this.connection.promise().query( 
+    //2. Role
+    findAllRoles() {
+        return this.connection.promise().query(
+            "SELECT * from role");
+
+    }
+    //3. Employee
+    findAllEmployees() {
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+
+    }
+    //4. Add Department
+    createDepartment(department) {
+        return this.connection.promise().query(
             "INSERT INTO department SET ?", department);
     }
 
-    //5. addRole
-    createRole(role){
-        return this.connection.promise().query( 
-            "INSERT * from employee ") ;
+    //5. Add Role
+    createRole(role) {
+        return this.connection.promise().query(
+            "INSERT INTO role SET ?", role);
     }
-    //6. addEmployee
-    createEmployee(employee){
-        return this.connection.promise().query( 
-            "INSERT INTO employee SET ?", employee) ;
+
+    //6. Add Employee
+    createEmployee(employee) {
+        return this.connection.promise().query(
+            "INSERT INTO employee SET ?", employee);
+    }
+
+    //7. Update Employee
+    updateEmployee(employee){
+        return this.connection.promise().query(
+            `UPDATE employee SET role_id = ? WHERE id = ?`
+        );
     }
 
 }
