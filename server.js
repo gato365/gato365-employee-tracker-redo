42960e2839db2ec905e24128517801e41083c28c
gato365 <immanuelwilliams@gato365.com>
2022-12-31 08:00:12 -0800

    Created update and post api

--- server.js
+++ server.js
@@ -55,14 +55,34 @@ app.get("/api/departments", (req, res) => {
     });
 });
 
+// Post
+app.post("/api/add-department", (req, res) => {
+    const { name } = req.body;
+    connection.query("INSERT INTO department(name) values (?)  ", name, (err, result) => {
+        res.status(200);
+        res.end();
+    });
+
+})
+
+// Update
+app.get("/api/department/:id/employee",(req,res) => {
+    const { id }  = req.params;
+    
+    connection.query("select * from employee WHERE role_id =?", role_id,
+     function (err, results) {
+        res.json(results);
+    });
+
+})
+
 
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
-  });
-  
-  app.listen(PORT, () => {
+});
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
-  
+});
