47810cde87d7da301a5ec90150a9e116f3d90368
gato365 <immanuelwilliams@gato365.com>
2022-12-31 07:28:06 -0800

    Added and understand kinda the get

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 require("dotenv").config();
+const express = require('express');
 
 const mysql = require("mysql2");
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -9,6 +23,46 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASS
 })
 
-connection.query("select * from department", function(err, results)  {
-    console.log(results);
+
+// -----------------Function Definitions--------------------
+// Author: Immanuel Williams PhD 
+// Date Created: 12/31/2022
+// Date Modified: 12/31/2022
+// Name: getDepartment
+// Purpose: Gets Department info
+// Input: NA
+// Output: NA
+// Notes: NA
+// -----------------Function Definitions--------------------
+
+
+function getDepartment() {
+    // Question 1: Funtions , Call Back Functions, Arrow Functions, Promise, Await & Async
+    return new Promise((resolve) => { // Question 2: Why is there not are reject?
+        connection.query("select * from department", function (err, results) {
+            resolve(results);
+            console.log(results);
+        });
+    });
+}
+
+
+
+app.get("/api/departments", (req, res) => {
+    getDepartment().then((allDepartments) => {
+        const allDepartmentsString = JSON.stringify(allDepartments);
+        res.send(allDepartments);
+    });
 });
+
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
